908ca0a705aed48a27dc751683e6dd73a7b8c4bd
Francisco <francisco.arruda01@hotmail.com>
2024-07-18 10:10:34 +0000

    Novo splash screen

--- App.js
+++ App.js
@@ -1,75 +1,48 @@
-import React from 'react';
-import { NavigationContainer, Easing } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Inicio from './pages/inicio.js';
-import DateInput from './pages/data_hora.js';
-import Perguntas from './pages/Inquerito.js';
-import Submeter from './pages/submeter.js';
-import Rgpd from './pages/consentimento.js';
-import Result from './pages/result.js';
-import Lista from './pages/lista.js';
-import Mapa from './pages/mapa.js';
-import Sismo from './pages/sismo.js';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Inicio from "./pages/inicio.js";
+import DateInput from "./pages/data_hora.js";
+import Perguntas from "./pages/Inquerito.js";
+import Submeter from "./pages/submeter.js";
+import Rgpd from "./pages/consentimento.js";
+import Result from "./pages/result.js";
+import Lista from "./pages/lista.js";
+import Mapa from "./pages/mapa.js";
+import Sismo from "./pages/sismo.js";
 
 const Stack = createStackNavigator();
 const App = () => {
-
   return (
-    <NavigationContainer >
-      <Stack.Navigator screenOptions={{
-        headerShown: false,
-        gestureEnabled: true,
-        transitionSpec: {
-          open: { animation: 'spring', config: { duration: 300 } },
-          close: { animation: 'spring', config: { duration: 300 } },
-        },
-        headerStyle: {
-          backgroundColor: '#FFFFFF',
-        },
-        cardStyle: {
-          backgroundColor: '#FFFFFF',
-        },
-      }}>
-        <Stack.Screen
-          name="Inicio"
-          component={Inicio}
-        />
-        <Stack.Screen
-          name="RGPD"
-          component={Rgpd}
-        />
-        <Stack.Screen
-          name="Data e Hora"
-          component={DateInput}
-        />
-        <Stack.Screen
-          name="Perguntas"
-          component={Perguntas}
-        />
-        <Stack.Screen
-          name="Submeter"
-          component={Submeter}
-        />
-        <Stack.Screen
-          name="Result"
-          component={Result}
-        />
-        <Stack.Screen
-          name="Lista"
-          component={Lista}
-        />
-        <Stack.Screen
-          name="Mapa"
-          component={Mapa}
-        />
-        <Stack.Screen
-          name="Sismo"
-          component={Sismo}
-        />
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          gestureEnabled: true,
+          transitionSpec: {
+            open: { animation: "spring", config: { duration: 300 } },
+            close: { animation: "spring", config: { duration: 300 } },
+          },
+          headerStyle: {
+            backgroundColor: "#FFFFFF",
+          },
+          cardStyle: {
+            backgroundColor: "#FFFFFF",
+          },
+        }}
+      >
+        <Stack.Screen name="Inicio" component={Inicio} />
+        <Stack.Screen name="RGPD" component={Rgpd} />
+        <Stack.Screen name="Data e Hora" component={DateInput} />
+        <Stack.Screen name="Perguntas" component={Perguntas} />
+        <Stack.Screen name="Submeter" component={Submeter} />
+        <Stack.Screen name="Result" component={Result} />
+        <Stack.Screen name="Lista" component={Lista} />
+        <Stack.Screen name="Mapa" component={Mapa} />
+        <Stack.Screen name="Sismo" component={Sismo} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-
 };
 
 export default App;
--- services/background.js
+++ services/background.js
@@ -1,35 +1,34 @@
 function backGround(value) {
   let n = value;
-  if (n && n.includes('/')) {
-    n = n.split('/')[1];
+  if (n && n.includes("/")) {
+    n = n.split("/")[1];
   }
   switch (n) {
-    case 'II':
-      return '#BFCCFF';
-    case 'III':
-      return '#9B9BFF';
-    case 'IV':
-      return '#88DCDC';
-    case 'V':
-      return '#7EF895';
-    case 'VI':
-      return '#FDBE01';
-    case 'VII':
-      return '#7EF895';
-    case 'VIII':
-      return '#FF711F';
-    case 'IX':
-      return '#FF0302';
-    case 'X':
-      return '#DC0C0C';
-    case 'XI':
-      return '#880201';
-    case 'XII':
-      return '#440203';
+    case "II":
+      return "#BFCCFF";
+    case "III":
+      return "#9B9BFF";
+    case "IV":
+      return "#88DCDC";
+    case "V":
+      return "#7EF895";
+    case "VI":
+      return "#FDBE01";
+    case "VII":
+      return "#7EF895";
+    case "VIII":
+      return "#FF711F";
+    case "IX":
+      return "#FF0302";
+    case "X":
+      return "#DC0C0C";
+    case "XI":
+      return "#880201";
+    case "XII":
+      return "#440203";
     default:
-      return '#FFFFFF';
+      return "#FFFFFF";
   }
-};
+}
 
-export default backGround
-
+export default backGround;
